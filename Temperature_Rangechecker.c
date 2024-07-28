149c27f3591cbc5d695c7c8f082146feffbb68a2
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 19:46:05 +0530

    Update Temperature_Rangechecker.c

--- Temperature_Rangechecker.c
+++ Temperature_Rangechecker.c
@@ -1,8 +1,8 @@
 #include "Temperature_Rangechecker.h"
 #include "ValueRange_checker.h"
-#include "EarlyWarnings.h"
+#include "EarlyWarning.h"
 
 int isTemperatureOk(float temperature) {
-    checkWarning(temperature, 0, 45, 2.25, "Warning: Approaching lower temperature limit", "Warning: Approaching upper temperature limit");
+    checkEarlyWarning(temperature, 0, 45, 2.25, "Warning: Approaching lower temperature limit", "Warning: Approaching upper temperature limit");
     return isValueInRange(temperature, 0, 45, "Temperature out of range!");
 }
