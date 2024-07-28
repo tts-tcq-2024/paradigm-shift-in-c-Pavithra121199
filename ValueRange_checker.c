3b87f443eeeb1fababa4904817489ac64e4de594
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 20:14:45 +0530

    Update ValueRange_checker.c

--- ValueRange_checker.c
+++ ValueRange_checker.c
@@ -8,3 +8,11 @@ int isValueInRange(float value, float min, float max, const char* WarningMessage
     }
     return 1;
 }
+
+int isChargeRateInRange(float value, float max, const char* WarningMessage) {
+    if (value > max) {
+        printWarningMessage(WarningMessage);
+        return 0;
+    }
+    return 1;
+}
