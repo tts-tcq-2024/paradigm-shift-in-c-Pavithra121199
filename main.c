5e789eecdf9882ad4da00982e25a0f232cd4b7ef
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 21:04:20 +0530

    Update main.c

--- main.c
+++ main.c
@@ -15,8 +15,8 @@ int main() {
     assert(!batteryIsOk(30, 75, 0.9));  // chargeRate above upper limit
 
     //Early warnings
-    assert(!batteryIsOk(2.25, 75, 0.8));  // Approaching lower temperature limit
-    assert(!batteryIsOk(42.75, 75, 0.8));  // Approaching higher temperature limit
+    assert(!batteryIsOk(2, 75, 0.8));  // Approaching lower temperature limit
+    assert(!batteryIsOk(43, 75, 0.8));  // Approaching higher temperature limit
     assert(!batteryIsOk(30, 24, 0.8));  // Approaching discharge
     assert(!batteryIsOk(30, 76, 0.8));  // Approaching charge-peak
     assert(!batteryIsOk(30, 60, 0.76));  // Approaching maximum charge rate
