9a8c86a23ca715ac47b6357014b6b6c83ce30515
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 22:19:46 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include "Battery_Monitor.h"
 
 int main() {
+    // Valid cases
+    assert(batteryIsOk(30, 75, 0.6));  // Valid case - random input
+    assert(batteryIsOk(0, 20, 0.8));  // Valid case checking the lower limit of temperature, soc and chargeRate
+    assert(batteryIsOk(45, 80, 0.8));  // Valid case checking the upper limit of temperature and soc
     
     // Invalid temperature cases
     assert(!batteryIsOk(-1, 75, 0.6));  // Temperature below lower limit
@@ -13,12 +17,14 @@ int main() {
     
     // Invalid chargeRate case
     assert(!batteryIsOk(30, 75, 0.9));  // chargeRate above upper limit
+    
+    // Early warnings
+    assert(batteryIsOk(2, 75, 0.6));  
+    assert(batteryIsOk(44, 75, 0.6));
+    assert(batteryIsOk(20, 21, 0.6));  
+    assert(batteryIsOk(20, 79, 0.6));  
+    assert(batteryIsOk(20, 40, 0.75));
+    
 
-    //Early warnings
-    assert(batteryIsOk(2.25, 75, 0.5));  // Approaching lower temperature limit
-    assert(batteryIsOk(42.75, 75, 0.5));  // Approaching higher temperature limit
-    assert(batteryIsOk(30, 24, 0.5));  // Approaching discharge
-    assert(batteryIsOk(30, 76, 0.5));  // Approaching charge-peak
-    assert(batteryIsOk(30, 60, 0.76));  // Approaching maximum charge rate
     return 0;
 }
