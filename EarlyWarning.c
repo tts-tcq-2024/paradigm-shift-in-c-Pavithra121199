b85ffe95092b9e532a6ce73c3217a2baa0367176
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 19:42:28 +0530

    Update EarlyWarning.c

--- EarlyWarning.c
+++ EarlyWarning.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 #include "EarlyWarning.h"
 
-void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning) {
+void printLowWarning(float value, float min, float tolerance, const char* lowWarning) {
     if (value >= min && value < min + tolerance) {
         printf("%s\n", lowWarning);
-    } else if (value > max - tolerance && value <= max) {
+    }
+}
+
+void printHighWarning(float value, float max, float tolerance, const char* highWarning) {
+    if (value > max - tolerance && value <= max) {
         printf("%s\n", highWarning);
     }
+}
+
+void checkEarlyWarning(float value, float min, float max, float tolerance, const char* lowWarning, const char* highWarning) {
+    printLowWarning(value, min, tolerance, lowWarning);
+    printHighWarning(value, max, tolerance, highWarning);
+}
+
+void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning) {
+    printHighWarning(value, max, tolerance, highWarning);
 }
