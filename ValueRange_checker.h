99a57b784de3de7222388beaaa9704b77f598703
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 20:18:28 +0530

    Update ValueRange_checker.h

--- ValueRange_checker.h
+++ ValueRange_checker.h
@@ -2,5 +2,6 @@
 #define VALUERANGE_CHECKER_H
 
 int isValueInRange(float value, float min, float max, const char* WarningMessage);
+int isChargeRateInRange(float value, float max, const char* WarningMessage);
 
 #endif 
