4cdddc362d9a422c5a9ee1b3d2d02e7e152fa83d
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 20:00:06 +0530

    Update and rename EarlyWarningForChargerate.c to EarlyWarningMessage_printer.c

--- EarlyWarningForChargerate.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "EarlyWarning.h"
-
-void checkEarlyWarningForChargeRate(float value, float max, float tolerance, const char* highWarning) {
-    if (value > max - tolerance && value <= max) {
-        printf("%s\n", highWarning);
-    }
-}
