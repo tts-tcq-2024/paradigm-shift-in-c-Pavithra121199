1f24bd27c00aa9910c687278d790c2096c0d2966
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-28 18:34:24 +0530

    main.c

--- main.c
+++ main.c
@@ -1,41 +1,5 @@
-#include <stdio.h>
 #include <assert.h>
-
-void printMessage(const char* Message) {
-    printf("%s\n", Message);
-}
-
-int isValueInRange(float value, float min, float max, const char* Message) {
-    if (value < min || value > max) {
-        printMessage(Message);
-        return 0;
-    }
-    return 1;
-}
-
-int isInRange(float value, float max, const char* Message) {
-    if (value > max) {
-        printMessage(Message);
-        return 0;
-    }
-    return 1;
-}
-
-int isTemperatureOk(float temperature) {
-    return isValueInRange(temperature, 0, 45, "Temperature out of range!");
-}
-
-int isSocOk(float soc) {
-    return isValueInRange(soc, 20, 80, "State of Charge out of range!");
-}
-
-int isChargeRateOk(float chargeRate) {
-    return isInRange(chargeRate, 0.8, "Charge Rate out of range!");
-}
-
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
-}
+#include "batteryIsOk.h"
 
 int main() {
     // Valid cases
@@ -59,5 +23,6 @@ int main() {
     assert(!batteryIsOk(30, 19, 0.9));  // soc and chargeRate out of range
     assert(!batteryIsOk(-1, 75, 0.9));  // temperature and chargeRate out of range
     assert(!batteryIsOk(-1, 19, 0.9));  // All parameters out of range
- return 0;
+
+    return 0;
 }
