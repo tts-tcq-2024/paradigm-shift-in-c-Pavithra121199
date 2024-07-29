2725d8eb1c147244bd963158a457833e5fdf7904
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-29 08:53:18 +0530

    Update SOC_RangeChecker.c

--- SOC_RangeChecker.c
+++ SOC_RangeChecker.c
@@ -6,3 +6,4 @@ int isSocOk(float soc) {
     checkEarlyWarning(soc, 20, 80, 4, "Warning: Approaching discharge", "Warning: Approaching charge-peak");
     return isValueInRange(soc, 20, 80, "State of Charge out of range!");
 }
+
